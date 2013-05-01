86ef7c4a08a22387f78470aeb65abfeae5ec1770
DJZhou <dingjun.zhou@gmail.com>
2013-05-01 16:06:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 apt-get -y update
-apt-get -y install build-essential zlib1g-dev libssl-dev libreadline5-dev libyaml-dev
-cd /tmp
-wget ftp://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
-tar -xvzf ruby-1.9.3-p194.tar.gz
-cd ruby-1.9.3-p194/
-./configure --prefix=/usr/local
-make
-make install
-gem install chef ruby-shadow --no-ri --no-rdoc
+apt-get -y install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev nodejs
+apt-get -y install vim tmux samba
+apt-get -y install ruby1.9.1 ruby1.9.1-dev
